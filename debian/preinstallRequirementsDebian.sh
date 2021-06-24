6b1883ace6dd0edeb847d1cceb76ae0da39e8718
amzy-0 <azymiamin@gmail.com>
2021-06-24 23:34:53 +0430

    license changed : GPL -> Apache 2

--- debian/preinstallRequirementsDebian.sh
+++ debian/preinstallRequirementsDebian.sh
@@ -1,3 +1,18 @@
+# Copyright [2021] [M.Amin Azimi .K (amzy-0)]
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 sudo apt update && sudo apt full-upgrade -y;
-sudo apt install gcc build-essential libgtk-3-dev libnotify-bin zenity libvte-2.91-dev python3-tk python3-pip -y;
+sudo apt install gcc build-essential libgtk-3-dev libnotify-bin zenity libvte-2.91-dev python3-tk strip python3-pip -y;
 pip3 install Pillow
