fdbcc72e0524a43527d18850c7a15f5b406be9d9
amzy-0 <azymiamin@gmail.com>
2021-06-30 04:05:14 +0430

    bug fixed : directory not found !

--- debian/install-heimdallr-debian.py
+++ debian/install-heimdallr-debian.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 ''' 
 
 
@@ -19,55 +19,55 @@ limitations under the License.
 '''
 
 from os import system as syscall
-syscall('sudo apt install -f') # fix all broken packages
+# syscall('sudo apt install -f') # fix all broken packages
 from os import getenv
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 
 syscall('mkdir -p ~/.heimdallr/makefile/ ; cp -rf ../* %s/.heimdallr/' % getenv('HOME'))
-syscall('cp -rf ../.* %s/.heimdallr/' % getenv('HOME'))
+syscall('cp -rf ../* %s/.heimdallr/' % getenv('HOME'))
 syscall('cp -rf Makefile %s/.heimdallr/makefile/' % getenv('HOME'))
 
 root = tk.Tk()
 progress = ttk.Progressbar(root, length = 100, mode = 'determinate')
-
+# 
 def installer_heimdallr():
     import time 
-    
+    # 
     progress['value'] = 20
     root.update_idletasks() 
     time.sleep(0.4) 
     syscall('make')
     syscall('sudo cp heimdallr /usr/bin/')
     syscall('sudo cp heimdallr-update /usr/bin/')
-    
+    # 
     progress['value'] = 50
     root.update_idletasks() 
     time.sleep(0.4)
     syscall('sudo cp -rf ../xdg/heimdallr.desktop /usr/share/applications')
     syscall('cp -rf ../xdg/heimdallr.desktop ~/.local/share/applications')
-
+# 
     progress['value'] = 70
     root.update_idletasks() 
     time.sleep(0.4)
     syscall("sudo cp -rf ../icon/heimdallr.png /usr/share/icons/hicolor/256x256/apps/")
     syscall("sudo cp -rf ../icon/heimdallr.png /usr/share/icons/hicolor/256x256/apps/")
     syscall("sudo cp -rf ../icon/heimdallr.png /usr/share/icons/")
-
+# 
     progress['value'] = 100
     root.update_idletasks() 
     time.sleep(0.4)
     syscall('zenity --info --title Heimdallr --text "Heimdallr installed !"')
     exit(0)
-
+# 
 img = ImageTk.PhotoImage(Image.open('../icon/heimdallr.png'))
 panel = tk.Label(root, image=img)
-
+# 
 # installer btn 
 installerBtn = tk.Button(root, text='INSTALL Heimdallr', command=installer_heimdallr)
 installerBtn.configure(fg='white', bg='black')
-
+# 
 root.configure(bg='black')
 root.geometry('400x400')
 root.resizable(False, False)
@@ -79,5 +79,6 @@ panel.pack(fill='both', expand='yes')
 progress.pack(fill='x')
 installerBtn.focus()
 installerBtn.pack(fill='x')
-
+# 
 root.mainloop()
+# 
